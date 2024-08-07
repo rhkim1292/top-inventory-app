3eca975cff9a00dfbc770b8d2dde4202a3aa2b83
Randy Kim <rhkim1292@gmail.com>
2024-08-06 23:56:06 -0700

    Implement post route/view for creating items

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,9 +1,8 @@
 const Item = require("../models/item");
 const Category = require("../models/category");
-
 const asyncHandler = require("express-async-handler");
-
 const { formatCurrency } = require("../lib/formatters");
+const { body, validationResult } = require("express-validator");
 
 exports.index = asyncHandler(async (req, res, next) => {
 	// Get details of books, book instances, authors and genre counts (in parallel)
@@ -50,15 +49,75 @@ exports.item_detail = asyncHandler(async (req, res, next) => {
 	});
 });
 
-// Display Item create form on GET.
+// Display item create form on GET.
 exports.item_create_get = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: Item create GET");
+	// Get all categories, which we can use for adding to our item.
+	const allCategories = await Category.find().sort({ name: 1 }).exec();
+
+	res.render("item_form", {
+		title: "Create Item",
+		categories: allCategories,
+	});
 });
 
-// Handle Item create on POST.
-exports.item_create_post = asyncHandler(async (req, res, next) => {
-	res.send("NOT IMPLEMENTED: Item create POST");
-});
+// Handle item create on POST.
+exports.item_create_post = [
+	// // Convert the category to an array.
+	// (req, res, next) => {
+	// 	if (!Array.isArray(req.body.category)) {
+	// 		req.body.category =
+	// 			typeof req.body.category === "undefined" ? [] : [req.body.category];
+	// 	}
+	// 	next();
+	// },
+
+	// Validate and sanitize fields.
+	body("name", "Name must not be empty.").trim().isLength({ min: 1 }).escape(),
+	body("category.*").escape(),
+	body("priceInCents", "The price of the item must be greater than 0.").isInt({
+		min: 1,
+	}),
+	body("quantity", "Quantity cannot be a negative number.").isInt({ min: 0 }),
+	// Process request after validation and sanitization.
+
+	asyncHandler(async (req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+
+		// Create a Item object with escaped and trimmed data.
+		const item = new Item({
+			name: req.body.name,
+			category: req.body.category,
+			priceInCents: req.body.priceInCents,
+			quantity: req.body.quantity,
+		});
+
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values/error messages.
+
+			// Get all categories for form.
+			const allCategories = await Category.find().sort({ name: 1 }).exec();
+
+			// Mark our selected categories as checked.
+			for (const category of allCategories) {
+				if (item.category.includes(category._id)) {
+					category.selected = "true";
+					break;
+				}
+			}
+			res.render("item_form", {
+				title: "Create Item",
+				categories: allCategories,
+				item: item,
+				errors: errors.array(),
+			});
+		} else {
+			// Data from form is valid. Save book.
+			await item.save();
+			res.redirect(item.url);
+		}
+	}),
+];
 
 // Display Item delete form on GET.
 exports.item_delete_get = asyncHandler(async (req, res, next) => {
